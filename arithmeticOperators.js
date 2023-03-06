85da3295fc45369ca02874b6ed7057a2cae4e0ed
taherabbas67 <mtahera@gitam.in>
2023-03-06 10:25:54 +0530

    arithmetic operators

---
+++ arithmeticOperators.js
@@ -0,0 +1,23 @@
+// Arithmetic Operators
+let x = 5;
+let y = 2;
+let z;
+//Adding
+z = x + y;
+// Subtracting
+z = x - y;
+//Multiplying
+z = x * y;
+//Dividing
+z = x / y;
+//Remainder
+z = x % y;
+// Increment
+x++;
+//Decrement
+y--;
+//Exponentiation
+z = x ** 2;
+z = Math.pow(x, 2);
+
+//Operator Precedence
---
+++ operators.js
@@ -0,0 +1,10 @@
+
+// Types of Operators
+
+// 1. Arithmetic Operators
+// 2. Assignment Operators
+// 3. Comparision Operators
+// 4. Logocal Operators
+// 5. Conditional Operators
+// 5. Type Operators
+
