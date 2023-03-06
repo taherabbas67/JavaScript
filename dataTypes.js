0a804c448ca961121c47934b4c20268031440b42
taherabbas67 <mtahera@gitam.in>
2023-03-06 10:10:29 +0530

    datatypesJS

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+let name = 'Taher Abbas';         //String
+let ph = 9398035079;              //Number
+let student = true;               //Boolean
+let college;                      //Undefined
+
+const car = {           //Object
+    type: "fiat",
+    model : "2019",
+    color : "red"
+};
+
+
+// 1. String
+// 2. Number 
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
